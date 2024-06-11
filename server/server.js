a89b04e635d59ba1b97975c37da97c5f48f96ff6
muriel_ganne <gannemuriel@gmail.com>
2024-06-11 20:58:52 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
